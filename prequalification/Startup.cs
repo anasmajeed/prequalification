d7c5419b311cc492251e832e873c7365b8571b97
Anas Majeed <anasmajeed.ch@gmail.com>
2019-07-15 11:38:14 +0500

    Add project files.

---
+++ prequalification/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prequalification.Startup))]
+namespace prequalification
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
